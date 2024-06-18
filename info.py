8f91cb76601db7188d2a3545d7526af3cebf4c99
RpCinemas <rpcinemas473@gmail.com>
2024-06-18 16:53:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,8 +18,8 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'TechVJBot')
-API_ID = int(environ.get('API_ID', '22182189'))
-API_HASH = environ.get('API_HASH', '5e7c4088f8e23d0ab61e29ae11960bf5')
+API_ID = int(environ.get('API_ID', '22293838'))
+API_HASH = environ.get('API_HASH', '5eb39d3199a80c0a72a37af5f51b7df6')
 BOT_TOKEN = environ.get('BOT_TOKEN', "6752100007:AAGiJKhjgz2qj1XEtZ0c9f2b75z7e5ZsHRU")
 
 # Bot settings
